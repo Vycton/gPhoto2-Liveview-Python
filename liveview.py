a6911a0c57f221484508b485c454d7031700d48c
Vincent van Aalten <vincent.aalten@hotmail.com>
2021-11-14 14:02:15 +0100

    Updated liveview.py to make it a viable import

--- liveview.py
+++ liveview.py
@@ -8,11 +8,12 @@ from PIL import Image, ImageTk
 JPEG_START_CODE = b'\xff\xd8'
 JPEG_END_CODE = b'\xff\xd9'
 
+
 class FrameReader():
-    """ 
+    """
     Object that reads jpegs from an mjpeg input stream, puts them in a queue
     and then displays them on a tk panel. Will not stop until the input does.
-    """ 
+    """
 
     def __init__(self, panel, instream):
         self.panel = panel
@@ -20,9 +21,9 @@ class FrameReader():
         self.framecount = 0
         self.frame_queue = Queue()
         self._started = False
-    
+
     def start_threads(self):
-        """ 
+        """
         Starts one thread to read input frames from the stream, and one to
         update the panel with those frames.
         """
@@ -47,11 +48,11 @@ class FrameReader():
         while frame:
             frame = self.read_jpeg()
             self.frame_queue.put(frame)
-            
-        self.frame_queue.put(None) # signal end of frames
-    
+
+        self.frame_queue.put(None)  # signal end of frames
+
     def read_jpeg(self):
-        """ 
+        """
         Reads a single jpeg from the stream.
         Adapted from https://github.com/aqiank/gphoto2-liveview-example/blob/master/main.c
         """
@@ -61,13 +62,13 @@ class FrameReader():
         code = self.instream.read(2)
         while code and code != JPEG_START_CODE:
             code = self.instream.read(2)
-    
+
         while code != JPEG_END_CODE:
             code = self.instream.read(2)
             frame_buffer += code
             if not code:
                 # Stream has been cut off
-                return None # Signal end
+                return None  # Signal end
         return BytesIO(frame_buffer)
 
     def update_image(self):
@@ -82,24 +83,33 @@ class FrameReader():
             self.panel.configure(image=img)
             self.panel.image = img
             frame = self.frame_queue.get()
-    
 
-# For our purposes, this is where the mjpeg stream will come from
-instream = sys.stdin.buffer
 
-# Setup tk panel with corresponding reader
-root = tk.Tk()
-panel = tk.Label(root)
-reader = FrameReader(panel, instream)
+def main():
+    """
+    Creates a panel and starts the framereader
+    """
+    # For our purposes, this is where the mjpeg stream will come from
+    instream = sys.stdin.buffer
 
-# Read a frame and pack the panel in order to have the right window dimensions
-frame = reader.read_jpeg()
-img = Image.open(frame)
-img = ImageTk.PhotoImage(img)
-panel.configure(image=img)
-panel.image = img
-panel.pack(side="bottom", fill="both", expand="yes")
+    # Setup tk panel with corresponding reader
+    root = tk.Tk()
+    panel = tk.Label(root)
+    reader = FrameReader(panel, instream)
 
-# Start the reader and tk's main loop
-reader.start_threads()
-tk.mainloop()
+    # Read a frame and pack the panel in order to have the right window dimensions
+    frame = reader.read_jpeg()
+    img = Image.open(frame)
+    img = ImageTk.PhotoImage(img)
+    panel.configure(image=img)
+    panel.image = img
+    panel.pack(side="bottom", fill="both", expand="yes")
+
+    # Start the reader and tk's main loop
+    reader.start_threads()
+    tk.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
